8ba3ffa76f2833c15a7bf891dabaf52e0a4d35dd
Roderick <roderick.montague@uoit.net>
2018-10-23 13:42:35 -0400

    Creation and Editing
    Created a Tokens.cpp file to along with the header, and am preparing for the game loop in main.cpp.

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@ References: Cplusplus.com - random_shuffle(http://www.cplusplus.com/reference/al
 */
 
 // GAME DEVELOPMENT WORKSHOP - PROJECT 2
-#include "Tokens.h"
+// #include "Tokens.h"
 
 #include <iostream>
 #include <random>
 #include <ctime>
 #include <string>
+#include <iomanip>
 
 const unsigned short int LEN(45);
 static unsigned short int ary[LEN] = { 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 7, 7, 7, 7, 8, 8, 8, 8, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13 }; // #13 is te sorry card
 
 int cardGet();
 void rules(int);
+void game();
 
 void rules(int m) // prints the rules for the game.
 {
@@ -55,10 +57,16 @@ int cardGet() // cardGet2 optimized
 	return ::ary[index - 1]; // index is added to before being sent back, so index - 1 is used for what needs to be returned.
 }
 
+void game()
+{
+
+}
+
 // int sorry()
 int main()
 {
 	std::string input;
+	unsigned short int pNum(0);
 
 	srand(time(0)); // seeding the randomizer.
 
@@ -105,6 +113,32 @@ int main()
 		}
 
 	} while (input == "");
+
+
+	do
+	{
+		std::cout << "Now that we have gotten the rules out of the way, I must ask you, how many people are playing? This game supports 2-4 players, so enter the amount of people playing:" << std::endl;
+		std::cout << "Players: ";
+
+		if (!(std::cin >> pNum)) // checks to see if a valid character was given
+		{
+			std::cout << "\nInvalid. Go Again.\n" << std::endl;
+			
+			// Removing the rest of the invalid input.
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+			pNum = 0;
+			continue;
+		}
+		if (pNum < 2 || pNum > 4) // if the player number is greater than or less than the range stated.
+		{
+			std::cout << "\nThis game only supports 2-4 players. Try again.\n" << std::endl;
+			pNum = 0;
+			continue;
+		}
+
+	} while (pNum == 0);
 
 	system("pause");
 	return 0;
