934411383339c105b6cccd925a293c7cc26d1a2f
CricketVA <jiminy.cao@gmail.com>
2018-10-23 12:32:40 -0400

    Edited Token into full class
    Added variables for start, safety zone, and home
    Added setters and getters and a constructor

--- Tokens.h
+++ Tokens.h
@@ -1,8 +1,47 @@
 #pragma once
 //reference: https://www.geeksforgeeks.org/c-classes-and-objects/
-struct TokenClass {
-	public:
-		int Player;
-		int LocationX;
-		int LocationY;
-};
+class TokenClass {
+public:
+	int Player;
+	bool Start
+	bool SafeZone
+	bool Home
+	int Location;
+
+	TokenClass(int p) {
+		Player = p;
+		setStart(true);
+		setSafeZone(false);
+		setHome(false);
+	}
+
+	void setStart(bool start){
+		Start = start;
+	}
+	void setSafeZone(bool safe) {
+		SafeZone = safe;
+	}
+	void setHome(bool home) {
+		Home = home;
+	}
+	void setLocation(int space) {
+		Location = space;
+	}
+
+	bool getStart() {
+		return Start;
+	}
+	bool getSafeZone() {
+		return SafeZone;
+	}
+	bool getHome() {
+		return Home;
+	}
+	int getLocation() {
+		return Location;
+	}
+	int getPlayer() {
+		return Player;
+	}
+
+}
